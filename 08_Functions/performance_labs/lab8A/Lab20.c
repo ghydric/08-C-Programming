1e903d8ec2b3f0ccd3011fa0d412b4b98b868b2e
Mitch Hydrick <gary.hydrickm@gmail.com>
2020-01-15 13:45:17 -0600

    completed lab20

---
+++ 08_Functions/performance_labs/lab8A/Lab20.c
@@ -0,0 +1,52 @@
+/*
+Author: Mitch Hydrick
+Date: 1/15/2020
+Title: Performance Lab 20: Function - "Healthy Substitutions"
+Synopsis:
+    1. Create the following function:
+        - int replace_character(char * string, const char findThisChar, const char replaceItWithThis);
+    2. Function must have the following:
+        a. Return Value - Number of characters replaced
+        b. Parameters:
+            - Pointer to a null-terminated string
+            - Character to find
+            - Character to replace it with
+        c. Purpose - Replace all occurences of findThisChar with replaceItWithThis
+        d. Use pre-defined return values as indicated in shell code
+*/
+
+// import necessary libraries
+#include <stdio.h>
+// initialize global scope string to be modified
+char string_to_modify[] = "The quick brown fox jumps over the lazy dog.";
+
+int replace_character(char * string, const char findThisChar, const char replaceItWithThis);
+
+int main(void){
+    int num_replaced = replace_character(&string_to_modify, 'o', '_');
+    printf("Replaced %d characters\nNew string: %s", num_replaced, string_to_modify);
+
+    return 0;
+} 
+
+int replace_character(char * string, const char findThisChar, const char replaceItWithThis){
+    if (string == NULL){
+        return -1;
+    } else {
+        int characters_replaced = 0;
+        for (*string; *string != NULL; *string++){
+            if (*string == findThisChar){
+                *string = replaceItWithThis;
+                characters_replaced++;
+            }
+        }
+        //for (int i = 0; string[i] != NULL; i++){
+        //    if (string[i] == findThisChar){
+        //        string[i] = replaceItWithThis;
+        //        characters_replaced++;
+        //    }
+        //}
+        return characters_replaced;
+    }
+    
+}
